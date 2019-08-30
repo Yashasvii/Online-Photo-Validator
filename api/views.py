73d16cfcab22ddc5d6b276b1f565f4f8e1eff763
yashasvi <yashpant20@gmail.com>
2019-08-30 10:45:23 +0545

    refactor

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import os.path
 
 # Create your views here.
 
@@ -6,4 +7,7 @@ from django.shortcuts import render
 
 def upload(request):
     context = {}
+    fileLists = sorted(os.listdir('/home/yashasvi/opencv/Online-Photo-Validator/images'))
+    for image in fileLists:
+        print("a")
     return render(request, 'api/index1.html', context)
