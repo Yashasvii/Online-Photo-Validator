61e8dd15cffdb8e2c79d6e06fe1acb3b42494bf3
yashasvi <yashpant20@gmail.com>
2019-09-21 16:34:53 +0545

    redirect home page to app page

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
+from django.http import HttpResponseRedirect
 
 from . import views
+
+def redirect_root(request):
+    return HttpResponseRedirect('/photoValidator/')
+
 
 urlpatterns = [
     path('photoValidator/', views.startPage, name='photoValidator'),
     path('upload/', views.upload, name='upload'),
+    path('', redirect_root),
 
 
 ]
